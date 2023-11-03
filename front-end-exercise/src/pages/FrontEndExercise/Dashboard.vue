970b291dfd729a96e4cea94ea5e793bc20bbd447
James Nathan <jameslibra108@gmail.com>
2023-11-03 15:49:48 +0800

    Add Whole Folder
    I hope it will work

---
+++ front-end-exercise/src/pages/FrontEndExercise/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h5 class="dash-board">{{ Title }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashboardPage",
+
+  data() {
+    return {
+      Title: "Dashboard",
+    };
+  },
+};
+</script>
+
+<style>
+.dash-board {
+  margin: 5px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: orangered;
+}
+</style>
